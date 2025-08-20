17ec2daf95ea9841f94483450bcc969f349f82ac
aysa-nia <aysa_mayahinia@comp.iust.ac.ir>
2025-08-20 20:30:59 +0330

    modify zero-shot script

--- scripts/zsclip_cupl/zeroshot.sh
+++ scripts/zsclip_cupl/zeroshot.sh
@@ -3,7 +3,7 @@ DATASET=$1
 SAMPLE=$2
 CUPL=$3
 EXP=output_zs_evaluation
-DATAPATH=/path/to/datasets/folder
+DATAPATH="/content/data"
 # Zeroshot CLIP evaluation
 # --seed is only a place holder
 python train.py --root ${DATAPATH} --seed 1 --trainer ZeroshotCLIP --dataset-config-file configs/datasets/${DATASET}.yaml --config-file configs/trainers/ProText/text_only_supervised/imagenet.yaml --output-dir ./${EXP}/${DATASET}_${SAMPLE}_${CUPL} --eval-only DATASET.SUBSAMPLE_CLASSES ${SAMPLE} TRAINER.PROTEXT.GPT_PATH ${CUPL}
